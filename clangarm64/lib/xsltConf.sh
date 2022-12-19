5d9adfd972f9b8969ee3abb2a44129b5b06451c3
Dennis Ameling <dennis@dennisameling.com>
2022-12-19 19:20:01 +0000

    pacman: add mingw-w64-clang-aarch64-asciidoc
    asciidoctor is not available for clangarm64 yet as it
    depends on ruby, which is also not available yet. Therefore
    we use asciidoc. While there is an asciidoc executable
    available in /usr/bin/asciidoc, it runs under x64 emulation
    which is very slow. Let's install the native
    mingw-w64-clang-aarch64-asciidoc which will significantly
    speed up our build.
    
    Signed-off-by: Dennis Ameling <dennis@dennisameling.com>

---
+++ clangarm64/lib/xsltConf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the xslt library
+#
+XSLT_LIBDIR="-L/clangarm64/lib"
+XSLT_LIBS="-lxslt -L/clangarm64/lib -lxml2 "
+XSLT_PRIVATE_LIBS="-lm"
+XSLT_INCLUDEDIR="-I/clangarm64/include"
+MODULE_VERSION="xslt-1.1.37"
