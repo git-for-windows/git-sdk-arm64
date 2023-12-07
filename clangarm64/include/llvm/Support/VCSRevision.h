c2aefaf8d285cb4351c3869f9977019d17d90deb
Git for Windows Build Agent <ci@git-for-windows.build>
2023-12-07 03:16:10 +0000

    Update 16 packages
    curl (8.4.0-1 -> 8.5.0-1)
    info (7.1-1 -> 7.1-2)
    libcurl (8.4.0-1 -> 8.5.0-1)
    mingw-w64-clang-aarch64-clang (17.0.6-2 -> 17.0.6-3)
    mingw-w64-clang-aarch64-clang-analyzer (17.0.6-2 -> 17.0.6-3)
    mingw-w64-clang-aarch64-clang-libs (17.0.6-2 -> 17.0.6-3)
    mingw-w64-clang-aarch64-clang-tools-extra (17.0.6-2 -> 17.0.6-3)
    mingw-w64-clang-aarch64-compiler-rt (17.0.6-2 -> 17.0.6-3)
    mingw-w64-clang-aarch64-curl-openssl-alternate (8.4.0-1 -> 8.5.0-1)
    mingw-w64-clang-aarch64-curl-winssl (8.4.0-1 -> 8.5.0-1)
    mingw-w64-clang-aarch64-gcc-compat (17.0.6-2 -> 17.0.6-3)
    mingw-w64-clang-aarch64-lld (17.0.6-2 -> 17.0.6-3)
    mingw-w64-clang-aarch64-llvm (17.0.6-2 -> 17.0.6-3)
    mingw-w64-clang-aarch64-llvm-libs (17.0.6-2 -> 17.0.6-3)
    perl-http-cookiejar (new: 0.014-1)
    perl-libwww (6.67-1 -> 6.72-1)
    
    Signed-off-by: Git for Windows Build Agent <ci@git-for-windows.build>

--- clangarm64/include/llvm/Support/VCSRevision.h
+++ clangarm64/include/llvm/Support/VCSRevision.h
@@ -1,2 +1,2 @@
-#define LLVM_REVISION "b7e223841689c31a2961a011d78760169cd77a9a"
+#define LLVM_REVISION "2bac694f024bd66b7fee85c811c79f58055cff21"
 #define LLVM_REPOSITORY "https://github.com/git-for-windows/MINGW-packages"
