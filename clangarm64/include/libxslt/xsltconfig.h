cbf9ddf1864affea6dad93d3c4144d7f8bd0ef75
Git for Windows Build Agent <ci@git-for-windows.build>
2024-06-21 16:16:29 +0000

    Update 7 packages
    mingw-w64-clang-aarch64-asciidoctor (new: 2.0.23-1)
    mingw-w64-clang-aarch64-gdbm (new: 1.19-3)
    mingw-w64-clang-aarch64-libxml2 (2.12.7-1 -> 2.12.8-1)
    mingw-w64-clang-aarch64-libxslt (1.1.40-1 -> 1.1.41-1)
    mingw-w64-clang-aarch64-libyaml (new: 0.2.5-2)
    mingw-w64-clang-aarch64-pdcurses (new: 4.4.0-1)
    mingw-w64-clang-aarch64-ruby (new: 3.1.5-1)
    
    Signed-off-by: Git for Windows Build Agent <ci@git-for-windows.build>

--- clangarm64/include/libxslt/xsltconfig.h
+++ clangarm64/include/libxslt/xsltconfig.h
@@ -20,21 +20,21 @@ extern "C" {
  *
  * the version string like "1.2.3"
  */
-#define LIBXSLT_DOTTED_VERSION "1.1.40"
+#define LIBXSLT_DOTTED_VERSION "1.1.41"
 
 /**
  * LIBXSLT_VERSION:
  *
  * the version number: 1.2.3 value is 10203
  */
-#define LIBXSLT_VERSION 10140
+#define LIBXSLT_VERSION 10141
 
 /**
  * LIBXSLT_VERSION_STRING:
  *
  * the version number string, 1.2.3 value is "10203"
  */
-#define LIBXSLT_VERSION_STRING "10140"
+#define LIBXSLT_VERSION_STRING "10141"
 
 /**
  * LIBXSLT_VERSION_EXTRA:
@@ -113,19 +113,6 @@ extern "C" {
 #define WITH_MODULES
 #endif
 #define LIBXSLT_DEFAULT_PLUGINS_PATH() "/clangarm64/lib/libxslt-plugins"
-#endif
-
-/**
- * ATTRIBUTE_UNUSED:
- *
- * This macro is used to flag unused function parameters to GCC
- */
-#ifdef __GNUC__
-#ifndef ATTRIBUTE_UNUSED
-#define ATTRIBUTE_UNUSED __attribute__((unused))
-#endif
-#else
-#define ATTRIBUTE_UNUSED
 #endif
 
 /**
--- clangarm64/lib/cmake/libxslt/libxslt-config.cmake
+++ clangarm64/lib/cmake/libxslt/libxslt-config.cmake
@@ -33,15 +33,15 @@
 #   LibXslt::LibExslt           - the libexslt library
 #   LibXslt::xsltproc           - the xsltproc command-line executable
 
-set(LIBXSLT_VERSION "1.1.40")
+set(LIBXSLT_VERSION "1.1.41")
 set(LIBXSLT_VERSION_MAJOR "1")
 set(LIBXSLT_VERSION_MINOR "1")
-set(LIBXSLT_VERSION_PATCH "40")
+set(LIBXSLT_VERSION_PATCH "41")
 
-set(LIBEXSLT_VERSION "0.8.21")
+set(LIBEXSLT_VERSION "0.8.22")
 set(LIBEXSLT_VERSION_MAJOR "0")
 set(LIBEXSLT_VERSION_MINOR "8")
-set(LIBEXSLT_VERSION_PATCH "21")
+set(LIBEXSLT_VERSION_PATCH "22")
 
 get_filename_component(PACKAGE_PREFIX_DIR ${CMAKE_CURRENT_LIST_DIR}/../../../ ABSOLUTE)
 
