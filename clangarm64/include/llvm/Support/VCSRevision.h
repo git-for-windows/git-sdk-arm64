34c702f35a6605f2c5f129c52702d390f665da86
Git for Windows Build Agent <ci@git-for-windows.build>
2025-05-17 03:34:45 +0000

    Update 9 packages
    mingw-w64-clang-aarch64-clang (20.1.3-2 -> 20.1.5-2)
    mingw-w64-clang-aarch64-clang-analyzer (20.1.3-2 -> 20.1.5-2)
    mingw-w64-clang-aarch64-clang-libs (20.1.3-2 -> 20.1.5-2)
    mingw-w64-clang-aarch64-clang-tools-extra (20.1.3-2 -> 20.1.5-2)
    mingw-w64-clang-aarch64-compiler-rt (20.1.3-2 -> 20.1.5-2)
    mingw-w64-clang-aarch64-gcc-compat (20.1.3-2 -> 20.1.5-2)
    mingw-w64-clang-aarch64-lld (20.1.3-2 -> 20.1.5-2)
    mingw-w64-clang-aarch64-llvm (20.1.3-2 -> 20.1.5-2)
    mingw-w64-clang-aarch64-llvm-libs (20.1.3-2 -> 20.1.5-2)
    
    Signed-off-by: Git for Windows Build Agent <ci@git-for-windows.build>

--- clangarm64/include/llvm/Support/VCSRevision.h
+++ clangarm64/include/llvm/Support/VCSRevision.h
@@ -1,2 +1,2 @@
-#define LLVM_REVISION "8df0c2fff4184deff15acce9bfd791fb6e0d60fe"
+#define LLVM_REVISION "2b52612de6e17dca767916784535c9ee67eb094f"
 #define LLVM_REPOSITORY "https://github.com/git-for-windows/MINGW-packages"
