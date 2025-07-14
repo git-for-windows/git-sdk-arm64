3d5d778b255ae5943553c616d9c98fc5db584ff4
Git for Windows Build Agent <ci@git-for-windows.build>
2025-07-14 03:59:30 +0000

    Update 23 packages
    mingw-w64-clang-aarch64-clang (20.1.7-2 -> 20.1.8-2)
    mingw-w64-clang-aarch64-clang-analyzer (20.1.7-2 -> 20.1.8-2)
    mingw-w64-clang-aarch64-clang-libs (20.1.7-2 -> 20.1.8-2)
    mingw-w64-clang-aarch64-clang-tools-extra (20.1.7-2 -> 20.1.8-2)
    mingw-w64-clang-aarch64-compiler-rt (20.1.7-2 -> 20.1.8-2)
    mingw-w64-clang-aarch64-crt-git (13.0.0.r57.gc1a7f831b-1 -> 13.0.0.r72.g7010671fa-1)
    mingw-w64-clang-aarch64-gcc-compat (20.1.7-2 -> 20.1.8-2)
    mingw-w64-clang-aarch64-headers-git (13.0.0.r57.gc1a7f831b-1 -> 13.0.0.r72.g7010671fa-1)
    mingw-w64-clang-aarch64-libc++ (20.1.7-1 -> 20.1.8-1)
    mingw-w64-clang-aarch64-libmangle-git (13.0.0.r57.gc1a7f831b-1 -> 13.0.0.r72.g7010671fa-1)
    mingw-w64-clang-aarch64-libunwind (20.1.7-1 -> 20.1.8-1)
    mingw-w64-clang-aarch64-libwinpthread (13.0.0.r57.gc1a7f831b-1 -> 13.0.0.r72.g7010671fa-1)
    mingw-w64-clang-aarch64-lld (20.1.7-2 -> 20.1.8-2)
    mingw-w64-clang-aarch64-lldb (20.1.7-1 -> 20.1.8-1)
    mingw-w64-clang-aarch64-llvm (20.1.7-2 -> 20.1.8-2)
    mingw-w64-clang-aarch64-llvm-libs (20.1.7-2 -> 20.1.8-2)
    mingw-w64-clang-aarch64-llvm-openmp (20.1.7-1 -> 20.1.8-1)
    mingw-w64-clang-aarch64-llvm-tools (20.1.7-2 -> 20.1.8-2)
    mingw-w64-clang-aarch64-polly (20.1.7-1 -> 20.1.8-1)
    mingw-w64-clang-aarch64-tools-git (13.0.0.r57.gc1a7f831b-1 -> 13.0.0.r72.g7010671fa-1)
    mingw-w64-clang-aarch64-winpthreads (13.0.0.r57.gc1a7f831b-1 -> 13.0.0.r72.g7010671fa-1)
    mingw-w64-clang-aarch64-winstorecompat-git (13.0.0.r57.gc1a7f831b-1 -> 13.0.0.r72.g7010671fa-1)
    vim (9.1.1365-1 -> 9.1.1492-1)
    
    Signed-off-by: Git for Windows Build Agent <ci@git-for-windows.build>

--- clangarm64/include/llvm/Support/VCSRevision.h
+++ clangarm64/include/llvm/Support/VCSRevision.h
@@ -1,2 +1,2 @@
-#define LLVM_REVISION "40d1e2d0273648632f1f02d3ca5e748db96de502"
+#define LLVM_REVISION "e5c3e0566471baba464a231910547b481d2181b9"
 #define LLVM_REPOSITORY "https://github.com/git-for-windows/MINGW-packages"
